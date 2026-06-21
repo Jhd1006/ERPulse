828ad3e46544dd8e32ff47a4dd078d449be8490c
Jhd1006 <doctor1006123@gmail.com>
2026-06-21 20:06:18 +0900

    Update Kubernetes version to 1.36

--- infra/variables.tf
+++ infra/variables.tf
@@ -37,7 +37,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.36"
 }
 
 # ========================= EKS 워커 노드 설정 =========================
