7f3a85a2a9e954d5eaded912afc547006ca97519
Jhd1006 <doctor1006123@gmail.com>
2026-07-11 21:18:50 +0900

    feat : BASE_URL 수정
    - ELB의 경우 apply 시마다 변경되므로, 수정 필요

--- load-test/high-load-test.js
+++ load-test/high-load-test.js
@@ -4,13 +4,14 @@ import { sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '20s', target: 15 },
-    { duration: '40s', target: 30 },
-    { duration: '40s', target: 30 },
+    { duration: '40s', target: 50 },
+    { duration: '10m', target: 50 },
     { duration: '20s', target: 0 },
   ],
 };
 
-const BASE_URL = 'http://a8ec4099aaa854dbdba109fda0ab4fb3-1852957901.ap-northeast-2.elb.amazonaws.com';
+// ELB 주소 확인: kubectl get svc erpulse-api -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+const BASE_URL = 'CHANGE_ME';
 
 export default function () {
   http.get(`${BASE_URL}/hospitals/`);
